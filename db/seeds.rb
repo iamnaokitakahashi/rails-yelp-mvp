c95280b70a2afcdc3058bda71d8cfde465a21c33
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-09 14:27:44 +0900

    fin making controller and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Cleaning database..."
+puts 'Cleaning database...'
 Restaurant.destroy_all
 
-puts "Creating restaurants..."
-5.times do |i|
-Restaurant.create!(
-  name: "Restaurant #{i + 1}",
-  address: "Address #{i + 1}",
-  phone_number: "123-456-789#{i}",
-  category: "Category #{i + 1}"
-)
+puts 'Creating restaurants...'
+restaurants = [
+  { name: 'Chinese Delighters', address: '123 Bruh St', phone_number: '555-1234-7777', category: 'chinese' },
+  { name: 'Italiano Pi Pi Za', address: '456 Ight St', phone_number: '555-5678-8888', category: 'italian' },
+  { name: 'Tokyo Drift', address: '789 Yessir St', phone_number: '555-9876-0909', category: 'japanese' },
+  { name: 'French Elegance', address: '321 Meme St', phone_number: '555-4321-6723', category: 'french' },
+  { name: 'Belgian Waffles', address: '654 Popo St', phone_number: '555-8765-1234', category: 'belgian' }
+]
+
+restaurants.each do |restaurant_attrs|
+  Restaurant.create!(restaurant_attrs)
 end
