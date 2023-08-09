94c6479cad671d458580e226d8c4e76d247c48df
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-09 12:26:07 +0900

    validated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+5.times do |i|
+Restaurant.create!(
+  name: "Restaurant #{i + 1}",
+  address: "Address #{i + 1}",
+  phone_number: "123-456-789#{i}",
+  category: "Category #{i + 1}"
+)
+end
