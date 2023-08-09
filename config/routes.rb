d33af8ccceb9986182eaaa8bfe4fbe8c5cbcaeab
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-09 16:45:54 +0900

    fin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  # get 'reviews/new'
-  # get 'reviews/create'
-  # get 'restaurants/index'
-  # get 'restaurants/show'
-  # get 'restaurants/new'
-  # get 'restaurants/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :restaurants, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
   end
